2bd3498e7bbca089b5725c6b09e3eae735473da6
Marcin Wrzos <marwrz36@gmail.com>
2020-03-08 12:28:04 +0100

    Zad.1 - Plot

--- lab01/lab01_wm44557.py
+++ lab01/lab01_wm44557.py
@@ -1,12 +1,17 @@
+from matplotlib import pyplot as plt
 def wartosc(xd):
     return 7.0 * pow(xd,2.0) + 5.0*xd + 7.0
 def funkcja():
-    tZero = -10;tN = 10; deltaT =(1.0/100); tn = tZero;n = 0
-   
+    tZero = -10; tN = 10; deltaT =(1.0/100); tn = tZero; n = 0
+    tab = []; tab2 = []
     while(tn <= tN):
         print(tn," ",wartosc(tn)," \n")
-
+        tab.append(tn)
+        tab2.append(wartosc(tn))
+        plt.plot(tab,tab2)
         tn=tZero + (n*deltaT)
         n=n+1
+        
 
 funkcja()
+plt.show()
