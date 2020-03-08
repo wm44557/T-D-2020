20c0cc3fb2175f076d3bcee0f9d86e7e1771d866
Marcin Wrzos <marwrz36@gmail.com>
2020-03-08 12:21:50 +0100

    Zad.1

--- lab01/lab01_wm44557.py
+++ lab01/lab01_wm44557.py
@@ -1,1 +1,12 @@
-123
+def wartosc(xd):
+    return 7.0 * pow(xd,2.0) + 5.0*xd + 7.0
+def funkcja():
+    tZero = -10;tN = 10; deltaT =(1.0/100); tn = tZero;n = 0
+   
+    while(tn <= tN):
+        print(tn," ",wartosc(tn)," \n")
+
+        tn=tZero + (n*deltaT)
+        n=n+1
+
+funkcja()
