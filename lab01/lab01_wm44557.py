0f20e6423e8e5d65565f59c0f30e7696a1e72155
Marcin Wrzos <marwrz36@gmail.com>
2020-03-08 16:24:07 +0100

    Kompletne Zadania + wykresy

--- lab01/lab01_wm44557.py
+++ lab01/lab01_wm44557.py
@@ -14,26 +14,26 @@ import math
 
 
 
-def wartosc(x):
-    x = 7.0 * pow(x,2.0) + 5.0*x + 7.0
-    return x
+# def wartosc(x):
+#     x = 7.0 * pow(x,2.0) + 5.0*x + 7.0
+#     return x
 
-def funkcja():
-    tZero = -10; tN = 10; deltaT =(1.0/100); tn = tZero; n = 0
-    tab = []; tab2 = []
-    while(tn <= tN):
-        print(tn," ",wartosc(tn)," \n")
-        tab.append(tn)
-        tab2.append(wartosc(tn))
-        plt.plot(tab,tab2)
-        tn=tZero + (n*deltaT)
-        n=n+1
+# def funkcja():
+#     tZero = -10; tN = 10; deltaT =(1.0/100); tn = tZero; n = 0
+#     tab = []; tab2 = []
+#     while(tn <= tN):
+#         print(tn," ",wartosc(tn)," \n")
+#         tab.append(tn)
+#         tab2.append(wartosc(tn))
+#         plt.plot(tab,tab2)
+#         tn=tZero + (n*deltaT)
+#         n=n+1
         
 
-funkcja()
-plt.show()
+# funkcja()
+# plt.show()
 
-###----------------------------------------------------------------------------###
+###-----------------------------------ZAD.2------------------------------------###
 def x(tn):
     return 7 * tn**2 + 5*tn + 5
 
