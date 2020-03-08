493ea044a1903f98391fdfb7b8e76ab61e22f7ca
Marcin Wrzos <marwrz36@gmail.com>
2020-03-08 16:23:16 +0100

    Kompletne zadania + wykresy

--- lab01/lab01_wm44557.py
+++ lab01/lab01_wm44557.py
@@ -2,36 +2,36 @@ from matplotlib import pyplot as plt
 import math
 # def check(xd):
 
-#     wFunkcji= 7 * xd**2 + 5*xd + 7
-#     bKwadrat = (5*xd)**2
-#     czteryAc = 4 * 7
-#     delta = bKwadrat-czteryAc
-#     iks1 = (-(5*xd) - math.sqrt(delta))/(2*7)
-#     iks2 = (-(5*xd) + math.sqrt(delta))/(2*7)
-#     if(delta < 0):
-#         return print("Brak miejsc zerowych")
-#     print("Wartosc funkcji: ", wFunkcji, "Delta: ", delta, "X.1: ", iks1, "X.2: ",iks2)
+    # wFunkcji= 7 * xd**2 + 5*xd + 7
+    # bKwadrat = (5*xd)**2
+    # czteryAc = 4 * 7
+    # delta = bKwadrat-czteryAc
+    # iks1 = (-(5*xd) - math.sqrt(delta))/(2*7)
+    # iks2 = (-(5*xd) + math.sqrt(delta))/(2*7)
+    # if(delta < 0):
+    #     return print("Brak miejsc zerowych")
+    # print("Wartosc funkcji: ", wFunkcji, "Delta: ", delta, "X.1: ", iks1, "X.2: ",iks2)
 
 
 
-# def wartosc(x):
-#     x = 7.0 * pow(x,2.0) + 5.0*x + 7.0
-#     return x
+def wartosc(x):
+    x = 7.0 * pow(x,2.0) + 5.0*x + 7.0
+    return x
 
-# def funkcja():
-#     tZero = -10; tN = 10; deltaT =(1.0/100); tn = tZero; n = 0
-#     tab = []; tab2 = []
-#     while(tn <= tN):
-#         print(tn," ",wartosc(tn)," \n")
-#         tab.append(tn)
-#         tab2.append(wartosc(tn))
-#         plt.plot(tab,tab2)
-#         tn=tZero + (n*deltaT)
-#         n=n+1
+def funkcja():
+    tZero = -10; tN = 10; deltaT =(1.0/100); tn = tZero; n = 0
+    tab = []; tab2 = []
+    while(tn <= tN):
+        print(tn," ",wartosc(tn)," \n")
+        tab.append(tn)
+        tab2.append(wartosc(tn))
+        plt.plot(tab,tab2)
+        tn=tZero + (n*deltaT)
+        n=n+1
         
 
-# funkcja()
-# plt.show()
+funkcja()
+plt.show()
 
 ###----------------------------------------------------------------------------###
 def x(tn):
