739b1294b5b4a99d9165fcba059e6d8ffa4d1cbb
Marcin Wrzos <marwrz36@gmail.com>
2020-03-08 15:18:29 +0100

    Wzór nr_3 + wykres

--- lab01/lab01_wm44557.py
+++ lab01/lab01_wm44557.py
@@ -34,31 +34,43 @@ import math
 # plt.show()
 
 ###----------------------------------------------------------------------------###
-def x(xd):
-    return 7 * xd**2 + 5*xd + 5
-
+def x(tn):
+    return 7 * tn**2 + 5*tn + 5
+def y(tn):
+    return 2 * (x(tn))**2 + 12 * math.cos(tn)
+def z(tn):
+    return math.sin((math.pi*2) * 7 * tn) * x(tn) - 0.2 * math.log10(abs(y(tn)) + math.pi)
+def u(tn):
+    return math.sqrt(abs(y(tn) * y(tn) * z(tn) )) - 1.8 * math.sin(0.4 * tn * z(tn) * x(tn) )
 
 def wartosc2(tn,opcja):
     if(opcja == 1):
-         a = 7.0 * pow(tn,2.0) + 5.0*tn + 5.0
+        wynik = y(tn)
     if(opcja == 2):
-         a = 2 * (x(tn))**2 + 12 * math.cos(tn)
-    return a
+        wynik = z(tn)
+    if(opcja == 3):
+        wynik = u(tn)
+
+    return wynik
     
     
 def funkcja2(opcja):
     tZero = 0; tN = 1; deltaT =(1.0/22050); tn = tZero; n = 0
     tab = []; tab2 = []
     while(tn <= tN):
-        print(tn," ",wartosc2(tn,opcja)," \n")
+        #print(tn," ",wartosc2(tn,opcja)," \n")
         tab.append(tn)
         tab2.append(wartosc2(tn,opcja))
         
         tn=tZero + (n*deltaT)
         n=n+1
     plt.plot(tab,tab2)
+    plt.figure()
+   
 
         
 
+funkcja2(opcja=1)
 funkcja2(opcja=2)
+funkcja2(opcja=3)
 plt.show()
