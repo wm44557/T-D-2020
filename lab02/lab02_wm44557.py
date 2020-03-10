3031ae80d076f6eb7a753335a4348f2a6660f9ef
Marcin Wrzos <marwrz36@gmail.com>
2020-03-11 00:17:52 +0100

    Zadania + wykresy

--- lab02/lab02_wm44557.py
+++ lab02/lab02_wm44557.py
@@ -5,12 +5,10 @@ from matplotlib import pyplot as plt
 fi = math.pi * 5
 A = 1.0
 fs = 100
-
 def sT(t):
     return A * math.sin(( 2*math.pi * 5 * t) +fi )
 
-
-def funkcja2():
+def funkcja1():
     tZero = 0; tN = 7; deltaT =(1.0/1000); tn = tZero; n = 0
     tab = []; tab2 = []
     while(tn <= tN):
@@ -27,10 +25,59 @@ def funkcja2():
     plt.plot(tab,tab2,'.')
     plt.xlabel('T')
     plt.ylabel('A')
+    plt.savefig('./lab02/wykres1.png')
+    
 
 
-   
+
+def sT2(t):
+    return round(((((A * math.sin(( 2*math.pi * 5 * t) +fi )) + 1) / 2*A) * 2**16),0)
+
+
+def funkcja2():
+    tZero = 0; tN = 7; deltaT =(1.0/1000); tn = tZero; n = 0
+    tab = []; tab2 = []
+    while(tn <= tN):
+        print(tn," ",sT2(tn)," \n")
+
+
+        tab.append(tn)
+        tab2.append( sT2(tn) )
+
+        tn=tZero + (n * deltaT)
+        n = n + 1
+
+    plt.figure()
+    plt.plot(tab,tab2,'.')
+    plt.xlabel('T')
+    plt.ylabel('A')
+    plt.savefig('./lab02/wykres2.png')
+
+def sT3(t):
+    return round(((((A * math.sin(( 2*math.pi * 5 * t) +fi )) + 1) / 2*A) * 2**8),0)
+
+def funkcja3():
+    tZero = 0; tN = 7; deltaT =(1.0/500); tn = tZero; n = 0
+    tab = []; tab2 = []
+    while(tn <= tN):
+        print(tn," ",sT3(tn)," \n")
+
+
+        tab.append(tn)
+        tab2.append( sT3(tn) )
+
+        tn=tZero + (n * deltaT)
+        n = n + 1
+
+    plt.figure()
+    plt.plot(tab,tab2,'.')
+    plt.xlabel('T')
+    plt.ylabel('A')
+    plt.savefig('./lab02/wykres3.png')
+
+
+funkcja1()
 funkcja2()
-plt.savefig('./lab02/foo2.png')
-
+funkcja3()
 plt.show()
+
---
+++ lab02/plik.py
@@ -0,0 +1,4 @@
+x = 5/2
+
+
+print(round(x,0))
